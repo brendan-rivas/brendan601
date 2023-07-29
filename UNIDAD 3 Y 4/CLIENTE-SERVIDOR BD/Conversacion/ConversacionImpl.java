87a738f067277849a610d879b70192d0a8ae4c35
brendan-rivas <133603398+brendan-rivas@users.noreply.github.com>
2023-07-29 14:50:08 -0600

    Se agrega ConversacionImpl
    Se agrega ConversacionImpl

---
+++ UNIDAD 3 Y 4/CLIENTE-SERVIDOR BD/Conversacion/ConversacionImpl.java
@@ -0,0 +1,50 @@
+package Conversacion;
+
+
+import Conexion.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConversacionImpl implements ConversacionDAO {
+
+    Conexion instanciaMySQL = Conexion.getInstance();
+
+    @Override
+    public void registrarConversacionA(String mensaje) {
+        PreparedStatement consulta = null;
+        Connection conexion = null;
+
+        try {
+            conexion = instanciaMySQL.conectar();
+            consulta = conexion.prepareStatement("insert into conversaciones(conversacionA) values(?)");
+
+            consulta.setString(1, mensaje);
+            consulta.executeUpdate();
+        } catch (SQLException error) {
+            System.err.println(error);
+            
+        }
+
+    }
+
+    @Override
+    public void registrarConversacionB(String mensaje) {
+
+        PreparedStatement consulta = null;
+        Connection conexion = null;
+
+        try {
+            conexion = instanciaMySQL.conectar();
+            consulta = conexion.prepareStatement("insert into conversaciones(conversacionB) values(?)");
+
+            consulta.setString(1, mensaje);
+            consulta.executeUpdate();
+        } catch (SQLException error) {
+            System.err.println(error);
+        }
+
+    }
+
+   
+}
