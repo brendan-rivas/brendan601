ee8ab1ab086fe5c407f2e1923bcc14067343ee5c
brendan-rivas <133603398+brendan-rivas@users.noreply.github.com>
2023-07-29 14:49:26 -0600

    Se agrega ConversacionDAO
    Se agrega ConversacionDAO

---
+++ UNIDAD 3 Y 4/CLIENTE-SERVIDOR BD/Conversacion/ConversacionDAO.java
@@ -0,0 +1,12 @@
+package Conversacion;
+
+public interface ConversacionDAO {
+
+public void registrarConversacionA(String mensaje);
+
+public void registrarConversacionB(String mensaje);
+
+    
+
+    
+}
